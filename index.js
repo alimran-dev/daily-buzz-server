67da0bd1c977dd8998361c42acda588f1b53fba0
alimran-dev <alimran5air@gmail.com>
2023-11-30 06:29:09 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(express.json());
 app.use(cors());
 
-
-app.get('/', (req, res) => {
-    res.send("Newspaper server is running...");
-})
-
-
+app.get("/", (req, res) => {
+  res.send("Newspaper server is running...");
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tklaef2.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,7 +22,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -33,8 +30,35 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+
+    //   jwt api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_KEY, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+    //   middleware
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -42,7 +66,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-})
+  console.log(`Server is running on port: ${port}`);
+});
